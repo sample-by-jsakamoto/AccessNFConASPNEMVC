629ff3b0cabc7489d4d758458681784e2eff8c8a
jsakamoto <junichi.jp@gmail.com>
2015-07-02 18:08:07 +0900

    1st commit.

---
+++ AccessNFC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace AccessNFC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var procStartInfo = new ProcessStartInfo
+            {
+                // set FileName = "ls" and Arguments = "~/*" will made available run on Raspberry Pi with XSP4 asp.net web server command, may be.
+                FileName = "cmd",
+                Arguments = "/c dir %windir%",
+
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var process = new Process { StartInfo = procStartInfo };
+            process.Start();
+            var outputStr = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return View((object)outputStr);
+        }
+    }
+}
